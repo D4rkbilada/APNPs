ab47b855cfffde0e7452c916ca0a1c3d3b1e8d45
Miguel Hernandes Arenhart do Canto <arenhartmiguel@gmail.com>
2020-10-19 18:09:11 -0300

    Arquivo index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+    
+    res.send("Olá Miguel Hernandes")
+    
+})
+
+app.listen(3000,function(){
+    
+    console.log("Conexão iniciada na porta 3000")
+    
+})
+
